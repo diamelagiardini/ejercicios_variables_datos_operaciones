e59da006aaac19f5c1d0a7bfc417efea51f7191b
diamelagiardini <diamelagiardini23@gmail.com>
2021-07-01 12:29:07 -0300

    agregue ejercicio 03

---
+++ 3.Datos_personales/app.js
@@ -0,0 +1,11 @@
+alert("Hola Usuario! Te voy a pedir algunos datos para agregarte al sistema.")
+
+let nombre = prompt("Ingresa tu nombre:");
+let apellido = prompt("Ingresa tu apellido:");
+let edad = prompt("Ingresa tu edad:");
+let nacionalidad = prompt("Ingresa tu nacionalidad:");
+let documento = prompt("Ingresa tu Numero de Identidad:");
+
+alert("Nuevo usuario agregado al sistema! " + "Nombre: " + nombre + " Apellido: " + apellido + " Edad: " + edad + " Nacionalidad: " + nacionalidad + " Numero de Identidad: " + documento)
+
+// Averiguar salto de linea en el alert
