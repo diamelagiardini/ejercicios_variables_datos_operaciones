a154204fc2cfb048921dd925cf9a89f996b5c76f
diamelagiardini <diamelagiardini23@gmail.com>
2021-07-01 12:59:29 -0300

    agregue ejercicio 05

---
+++ 5.Direccion_completa/app.js
@@ -0,0 +1,10 @@
+alert("Hola Usuario! Te voy a pedir algunos datos para dejar cargado en sistema tu direccion")
+
+let calle = prompt("Ingresa tu Calle:");
+let numeracion = prompt("Ingresa tu Numeracion:");
+let departamento = prompt("Ingresa tu Numero de Departamento:");
+let codigoPostal = prompt("Ingresa tu Codigo Postal:");
+let ciudad = prompt("Ingresa tu Ciudad:");
+let pais = prompt("Ingresa tu Pais:");
+
+alert("La dirección que ha ingresado es: " + calle + ", " + numeracion + ", " + departamento + "," + codigoPostal + ", " + ciudad + ", " + pais + "." + " Se ha guardado con exito!")
