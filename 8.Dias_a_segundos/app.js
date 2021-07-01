b94eadd0797bfb301fe09766f9eab1bd0f685431
diamelagiardini <diamelagiardini23@gmail.com>
2021-07-01 20:26:16 -0300

    agregue ejercicio 08

---
+++ 8.Dias_a_segundos/app.js
@@ -0,0 +1,8 @@
+alert("Hola usuarie! Vamos a convertir los dias en segundos.");
+
+let dias = Number(prompt("Ingresa una cantidad de dias:"));
+let horasDelDia = 24;
+let conversionSegundos = (60 * horasDelDia) * dias;
+
+alert("Ingresaste " + dias + " dias y son " + conversionSegundos + " segundos.")
+
