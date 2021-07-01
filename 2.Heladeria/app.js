4b2e961397fb41a97f7130fbb1abc656395e3b58
diamelagiardini <diamelagiardini23@gmail.com>
2021-06-30 22:07:08 -0300

    agregue ejercicio 02

---
+++ 2.Heladeria/app.js
@@ -0,0 +1,7 @@
+alert('Hola! Bienvenide a la Heledaria Imaginaria "Gustitos JS 🍦". Te voy a pedir tres gustos de helado en el siguiente paso.')
+
+let gusto1 = prompt("Ingresa el primer gusto");
+let gusto2 = prompt("Ingresa el segundo gusto");
+let gusto3 = prompt("Ingresa el tercer gusto");
+
+alert("Aca tenes tu helado super imaginario de " + gusto1 + ", " + gusto2 + "y " + gusto3 + "! Que los disfrutes 💜");
