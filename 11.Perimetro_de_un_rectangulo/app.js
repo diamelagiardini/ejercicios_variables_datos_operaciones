dc63ce557e04acc90865ec3ab5cd0b66fea0042b
diamelagiardini <diamelagiardini23@gmail.com>
2021-07-01 20:53:36 -0300

    agregue ejercicio 11

---
+++ 11.Perimetro_de_un_rectangulo/app.js
@@ -0,0 +1,8 @@
+alert("Vamos a calcular el Perimetro de un Rectangulo");
+
+let base = prompt("Ingresa la base del rectangulo:");
+let altura = prompt("Ingresa la altura del rectangulo:"); 
+
+let perimetro = (base * 2) + (altura * 2); 
+
+alert("El perimetro de tu rectangulo es " + perimetro + ".")
