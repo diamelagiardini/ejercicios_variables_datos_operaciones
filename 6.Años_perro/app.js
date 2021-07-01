d160b86d2feec42e0624546279d4ac2327d09a85
diamelagiardini <diamelagiardini23@gmail.com>
2021-07-01 16:40:08 -0300

    agregue ejercicio 06

---
+++ 6.Años_perro/app.js
@@ -0,0 +1,7 @@
+alert("Hola usuarie, veamos cuantos años tiene tu perro pasandolo a años humanos! ")
+
+let edadPerro = prompt("Cuantos años tiene tu perro?");
+let multiplicacion = 7;
+let equivalenteEnhumano = edadPerro * multiplicacion;
+
+alert("La edad de tu perro en años humanos es " + equivalenteEnhumano + "🐶")
