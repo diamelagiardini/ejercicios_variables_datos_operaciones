3cd2c19c040513c5eaaaefbc2a2cb066769507ef
diamelagiardini <diamelagiardini23@gmail.com>
2021-06-30 21:42:02 -0300

    agregue ejercicio 01

---
+++ 1.Saludo/app.js
@@ -0,0 +1,6 @@
+let nombreDelUsuario = prompt("Escribe tu nombre");
+let apellidoDelUsuario = prompt("Escribe tu apellido");
+
+let saludoNombreCompleto = "Hola " + nombreDelUsuario + " " + apellidoDelUsuario + ", Bienvenide a mis practicas 🤓";
+
+alert(saludoNombreCompleto);
