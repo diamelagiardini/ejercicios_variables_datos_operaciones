36452a8764d453dabc129d393ff0891f9a4a3c69
diamelagiardini <diamelagiardini23@gmail.com>
2021-07-01 20:07:27 -0300

    agregue ejercicio 07

---
+++ 7.Minutos_a_segundos/app.js
@@ -0,0 +1,6 @@
+alert("Hola! Vamos a convertir los minutos en segundos.")
+
+let cantidadMinutos = prompt("Ingresa una cantidad de minutos:");
+let conversionSegundos = 60 * cantidadMinutos; 
+
+alert("Has ingresado " + cantidadMinutos + " minutos y son " + conversionSegundos + " segundos.");
