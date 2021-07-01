08e6070197e63d344aabd6565b2e5a82abb49e0d
diamelagiardini <diamelagiardini23@gmail.com>
2021-07-01 12:51:13 -0300

    agregue ejercicio 04

---
+++ 4.Lista_de_reproduccion/app.js
@@ -0,0 +1,8 @@
+alert("Seccion Music 🎵")
+
+let playlist = prompt("Como queres llamar a tu Playlist?")
+let cancion1 = prompt("Decime el titulo de la primer cancion");
+let cancion2 = prompt("Decime el titulo de la segunda cancion");
+let cancion3 = prompt("Decime el titulo de la tercer cancion"); 
+
+alert("'" + playlist + "'" + " con las canciones " + "'" +cancion1 + "'" + ", " + "'" + cancion2 + "'" + " y " + "'" + cancion3 + "'")
