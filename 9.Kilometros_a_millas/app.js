aeb0d92d829657cf2d428c09a916ca73995821f2
diamelagiardini <diamelagiardini23@gmail.com>
2021-07-01 20:40:44 -0300

    agregue ejercicio 09

---
+++ 9.Kilometros_a_millas/app.js
@@ -0,0 +1,6 @@
+alert("Hola usuarie! Vamos a convertir los kilometros en millas.");
+
+let kilometros = prompt("Ingresa una cantidad de Km:");
+let conversionMillas = 0.621371 * kilometros;
+
+alert("Ingresaste " + kilometros + " Km, y son " + conversionMillas + " Millas.");
