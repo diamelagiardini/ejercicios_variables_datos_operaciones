f1f4211e8e4df4cea464f623c55bb0f0653d0194
diamelagiardini <diamelagiardini23@gmail.com>
2021-07-01 20:48:32 -0300

    agregue ejercicio 10

---
+++ 10.Area_de_un_triangulo/app.js
@@ -0,0 +1,7 @@
+alert("Vamos a calcular el Area de un Triangulo")
+
+let altura = prompt("Ingresa la altura de un triangulo:");
+let base = prompt("Ingresa la base de un triangulo");
+let area = (base * altura) / 2;
+
+alert("El area de su triangulo es " + area + ".")
