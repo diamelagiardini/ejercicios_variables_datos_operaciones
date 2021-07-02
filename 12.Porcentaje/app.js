307ea52f3b470f407dbb17332beb9404668f02ea
diamelagiardini <diamelagiardini23@gmail.com>
2021-07-01 21:06:43 -0300

    agregue ejercicio 12

---
+++ 12.Porcentaje/app.js
@@ -0,0 +1,8 @@
+alert("Vamos a sacar el porcentaje de un numero!");
+
+let numero = prompt("Ingresa un numero:");
+let porcentaje = prompt("Ingresa el porcentaje que deseas obtener del numero anterior:")
+
+let resultado = (numero * porcentaje) / 100;
+
+alert("El " + porcentaje + "% de " + numero + " es " + resultado);
